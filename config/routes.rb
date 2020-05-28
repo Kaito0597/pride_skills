469c52ffef10b7778121fcabf1d9139374a5e171
Dylan Thielking <dylangt0512@gmail.com>
2020-05-28 18:15:06 -0500

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
   get '/register', controller: 'application', action: 'register', as: 'register'
   get '/config', controller: 'application', action: 'configuration', as: 'config'
   get '/change_char', controller: 'application', action: 'change_character', as: 'change_character'
+  get '/about', controller: 'application', action: 'about', as: 'about'
 
 end
