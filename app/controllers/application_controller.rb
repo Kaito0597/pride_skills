b89401f788b838c592599fed666446259f181dda
Dylan Thielking <dylangt0512@gmail.com>
2020-05-28 18:36:14 -0500

    Small commit to test new deploy flow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-    before_action :authorized, :only => [:home, :configuration]
+    before_action :authorized, :only => [:home, :configuration. :about]
 
     helper_method :current_user
     helper_method :logged_in?
