452ed40299dccf9679b2f1770b47f17e2d3cefcf
Dylan Thielking <dylangt0512@gmail.com>
2020-06-03 21:29:55 -0500

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,6 +37,7 @@ class ApplicationController < ActionController::Base
     end
 
     def change_character
+        @user = User.find_by(:id => User.find_by(:id => session[:user_id]))
         @user.player_data.find_by(:game_id => user.selected_game_id).update(:selected_character_id => params[:id])
         
         redirect_to root_path
